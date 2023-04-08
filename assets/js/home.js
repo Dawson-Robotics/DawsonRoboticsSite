ab90b08f10c360f30f8fb1b84217a3e17de0918e
hoenggung888 <105127658+hoenggung888@users.noreply.github.com>
2023-04-08 18:17:49 -0400

    loading screen, optimization

--- assets/js/home.js
+++ assets/js/home.js
@@ -1,13 +1,15 @@
 var toggleTheme = document.getElementById("contentThemeSelection");
 var homeSlideshow = document.getElementById("homeSlides");
 var homeContent = document.getElementById("content");
-var sponsorSlideshow = document.getElementById("sponsorSlideshow");
 var headChange = document.getElementById("headerDesktop");
 var mobileLogoPopUp = document.getElementById("mobileLogo");
 var quickLinks = document.getElementById("quickLinks");
 var footerSite = document.getElementsByTagName("footer")[0];
+var linking = document.getElementsByClassName("links");
+var loading = document.getElementById("loadingCover");
 
 const gearNavi = '<nav class="gear"><img src="'+baseurl+'assets/images/gears/gear.png"></nav>';
+const spinnieGear = '<img src="'+baseurl+'assets/images/gears/gear.png">';
 const roboticsLogo = '<a href="'+baseurl+'" class="logo"><img src="'+baseurl+'assets/images/logos/DawsonRoboticsLogoGray.png" class="logo"></a>';
 const roboticsLogoMobile = '<img src="'+baseurl+'assets/images/logos/DawsonRoboticsLogo.png" class="homePageLogo">';
 const smallRoboticsLogo = '<img class="smallLogo" src="'+baseurl+'assets/images/logos/DawsonRoboticsSmallLogoBlueFill.png">'
@@ -39,15 +41,19 @@ function mobileLogo() {
 }
 
 function quickLinkSection() {
-    quickLinks.innerHTML='<h2>Quick Links</h2>'+'<a href="https://www.dawsoncollege.qc.ca" class="links">Dawson College</a>'+'<br></br>'+'<a href="https://space.dawsoncollege.qc.ca" class="links">SPACE</a>'+'<br></br>'+'<a href="https://robo-crc.ca" class="links">CRC</a>';
+    quickLinks.innerHTML='<h2>Quick Links</h2>'+
+    '<a href="https://www.dawsoncollege.qc.ca" class="links">Dawson College</a>'+
+    '<br></br>'+'<a href="https://space.dawsoncollege.qc.ca" class="links">SPACE</a>'+
+    '<br></br>'+'<a href="https://robo-crc.ca" class="links">CRC</a>';
 }
 
 function footerForm() {
     footerSite.innerHTML = '<h4>Copyright '+year+' Dawson Robotics. All rights reserved.</h4><br>';
 }
 
-function sponsorShow() {
-    sponsorSlideshow.innerHTML = slideSponsorsShow;
+function loaderScreen() {
+    loading.innerHTML = '<div class="spinning">'+spinnieGear+'</div>';
+    loading.style.display = "block";
 }
 
 headering();
@@ -56,5 +62,11 @@ slideshowing();
 mobileLogo();
 quickLinkSection();
 footerForm();
-sponsorShow();
 
+console.log(linking.length);
+
+for (var i = 0; i < linking.length; i++) {
+    linking[i].addEventListener("click", function() {
+        loaderScreen();
+    });
+  }
